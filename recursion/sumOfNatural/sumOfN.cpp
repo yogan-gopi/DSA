b4114e9946530283458d65b8c9b1349e497134c5
yogan-gopi <yogang2002@gmail.com>
2022-08-31 20:55:54 +0530

    updated sum of N

--- recursion/sumOfNatural/sumOfN.cpp
+++ recursion/sumOfNatural/sumOfN.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int fun(int n){
+int sum(int n){
     if(n > 0){
-        return fun(n-1)+n;
+        return sum(n-1)+n;
     }
     return 0;
 }
 
+int Isum(int n){
+    int sum = 0;
+    for(int i = 1; i <= n; i++)
+        sum += i;
+    return sum;
+}
 int main(){
-    cout<<fun(6);
+    cout<<sum(6)<<endl;
+    cout<<Isum(6);
     return 0;
 }
