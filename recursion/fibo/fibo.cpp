aaf939729f5e4c9db6ecf5393bc6fa6a93e6917c
yogan-gopi <yogang2002@gmail.com>
2022-09-01 20:13:24 +0530

    updated fibo

--- recursion/fibo/fibo.cpp
+++ recursion/fibo/fibo.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 int fibo(int n){
     if(n <= 1)
-        return 1;
+        return n;
     else
         return fibo(n-1)+fibo(n-2);
 }
 
 int main(){
-    cout << fibo(7);
+    cout << fibo(5) << endl;
+    cout << fibo(6) << endl;
+    cout << fibo(7) << endl;
     return 0;
 }
